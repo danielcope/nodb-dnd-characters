668236adbf5ded12a253704239ebf3da42286f4b
Daniel Cope <danielcope@users.noreply.github.com>
2021-03-11 23:26:56 -0800

    added just a couple of quick changes to CSS

--- src/Components/AddChar.js
+++ src/Components/AddChar.js
@@ -10,13 +10,13 @@ class AddChar extends Component {
   render (){
     return(
     <section className="add-info">
-      <h1>Add You Character!</h1>
+      <h1>Add Your Character!</h1>
       <section>
         <div className="input-list">Name: <input className="name"/></div>
         <div className="input-list">Race: <input className="race"/></div>
         <div className="input-list">Class: <input className="class"/></div>
         <div className="input-list">Weapon: <input className="weapon"/></div>
-        <div className="input-list">Image: <input className="img-add" placeholder="add image URL" /></div>
+        <div className="input-list">Image: <input className="img-add" placeholder="Add image URL" /></div>
         <section className="ac-hp">
           <div className="input-list">AC: <input className="ac"/></div>
           <div className="input-list">Max HP: <input className="maxhp"/></div>
